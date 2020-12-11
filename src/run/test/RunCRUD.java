1d5cef29c9642eb13886d65e21ebc01047c33bdf
chawanghyeon <chawanghyeon@gmail.com>
2020-12-11 16:35:37 +0900

    update

--- src/run/test/RunCRUD.java
+++ src/run/test/RunCRUD.java
@@ -1,21 +1,5 @@
 package run.test;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-
-import org.junit.jupiter.api.Test;
-
-import util.PublicCommon;
-
 public class RunCRUD {
 
-	@Test
-	public void run() {
-		EntityManager em = PublicCommon.getEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
-		
-		tx.commit();
-		em.close();
-	}
 }
