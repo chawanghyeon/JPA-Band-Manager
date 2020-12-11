e4891d09a514ea3b1ed64f0369a32b78162c7146
chawanghyeon <chawanghyeon@gmail.com>
2020-12-11 12:17:44 +0900

    update

--- src/model/domain/Member.java
+++ src/model/domain/Member.java
@@ -1,9 +1,12 @@
 package model.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
@@ -28,13 +31,19 @@ public class Member {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GEN")
 	private Long memberId;
 
+	@Column(nullable=false, length=20)
 	private String name;
 
+	@Column(nullable=false, length=20)
 	private String role;
 
+	@Column(nullable=false, length=20)
 	private String nationality;
 
+	@Column(nullable=false, length=10)
 	private String gender;
 
+	@ManyToOne
+	@JoinColumn(name="")
 	private Band bandId;
 }
---
+++ src/run/test/RunCRUD.java
@@ -0,0 +1,8 @@
+package run.test;
+
+public class RunCRUD {
+
+	public static void main(String[] args) {
+
+	}
+}
